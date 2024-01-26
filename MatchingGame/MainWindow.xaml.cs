a74e8158372d6041c5df405237f8d2bfb42d52fb
Ricardo <Ricardo@DESKTOP-LGUKO7O>
2024-01-26 16:32:57 -0300

    FIX: erro ao resetar jogo

--- MatchingGame/MainWindow.xaml.cs
+++ MatchingGame/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MatchingGame
 {
@@ -60,6 +54,7 @@ namespace MatchingGame
             {
                 if (textBlock.Name != "timeTextBlock")
                 {
+                    textBlock.Visibility = Visibility.Visible;
                     int index = random.Next(animalEmoji.Count);
                     string nextEmoji = animalEmoji[index];
                     textBlock.Text = nextEmoji;
